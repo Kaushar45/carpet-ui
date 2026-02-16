4129949a0de2a121c1702e4c9a6446553b5ba2ad
Kaushar45 <45kaushar@gmail.com>
2026-02-16 08:14:07 +0530

    add authentication pages and update routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import Navbar from "./component/Navbar";
 
 function App() {
   return (
     <>
       <div>
-        {/* <Navbar /> */}
+        <Navbar />
         <h1 className="text-amber-300 ">Hello</h1>
       </div>
     </>
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
+
 import Home from "../pages/Home";
 import About from "../pages/About";
 import NotFound from "../NotFound";
+import Signup from "../auth/Signup";
+import Login from "../auth/Login";
+import ForgotPassword from "../auth/ForgotPassword";
+import ResetPassword from "../auth/ResetPassword";
 
 export const router = createBrowserRouter([
   { path: "/", element: <Home /> },
+  { path: "/signup", element: <Signup /> },
+  { path: "/login", element: <Login /> },
+  { path: "/forgot-password", element: <ForgotPassword /> },
+  { path: "/reset-password", element: <ResetPassword /> },
   { path: "/about", element: <About /> },
   { path: "*", element: <NotFound /> },
 ]);
